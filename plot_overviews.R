b8ceed5725ed3e500273b6c5ae8f2c4c54dfea10
Cas Blaauw <38132585+CasBlaauw@users.noreply.github.com>
2021-12-06 22:49:55 +0100

    Update plot script

--- plot_overviews.R
+++ plot_overviews.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+# --- Histogram of the different cell type counts
+
 rctd_results <- read.table("results_rctd_table.csv", sep = ',', header = TRUE, row.names = 1)
 
 # Number of types above 30 beads (vline coordinate)
@@ -12,18 +14,25 @@ rctd_results %>% filter(spot_class %in% c('doublet_certain', 'doublet_uncertain'
 rctd_results %>% filter(spot_class %in% c('doublet_certain', 'doublet_uncertain', 'reject')) %>% count(types) %>% filter(n <= 30) %>% pull(n) %>% sum()
 
 rctd_hist <- rctd_results %>%
+  # Process data
   filter(spot_class %in% c('doublet_certain', 'doublet_uncertain', 'reject')) %>%
   count(types) %>%
   arrange(desc(n)) %>%
   mutate(types = as_factor(types),
          included = n > 30) %>%
+  
+  # Make base plot
   ggplot() +
   geom_bar(aes(types, n), size = 0, stat = 'identity', col = 'gray30', fill = 'gray30') + 
   # geom_hline(yintercept = 30) + 
   geom_vline(xintercept = 84, size = 1, linetype = 'longdash', alpha = 0.5) +
+
+  # Adjust plot elements
   theme_minimal() + 
   theme(axis.ticks.x = element_blank(), axis.text.x = element_blank(), panel.grid = element_blank()) +
   labs(x = 'Doublet type', y = 'Number of beads') + 
+
+  # Add text and curves
   annotate('text', x = 50, y = 10300, label = glue::glue('16/26: 10797 beads'), color = 'gray20') +
   geom_curve(
     aes(x = 37, y = 9950, xend = 3, yend = 9250),
@@ -38,31 +47,20 @@ rctd_hist <- rctd_results %>%
   )
 ggsave('rctd_type_hist.png', rctd_hist, width = 7.5, height = 3, units = 'in')
 
+
+# --- Barplot of marker presence in classes
 all_markers <- read_csv('all_markers.csv')
 
-# marker_pct <- all_markers %>%
-#   mutate(diff = pct.2 - pct.1) %>%
-#   pivot_longer(c(pct.1, pct.2), names_to = 'pct.type', values_to = 'pct') %>%
-#   mutate(ID = tidytext::reorder_within(ID, diff, types)) %>%
-#   ggplot() +
-#   geom_col(aes(x = ID, y = pct, fill = pct.type), position = 'dodge') +
-#   facet_wrap(~types, scales = 'free_x', nrow = 6) +
-#   theme_bw() +
-#   theme(axis.text.x = element_text(angle = 90),
-#         strip.background = element_rect(fill = 'white'),
-#         legend.position = c(0.865, 0.08),
-#         legend.title.align = 0.5) +
-#   ylab('Detection fraction') +
-#   scale_fill_discrete(name = 'Type', labels = c('Type 1', 'Type 2')) +
-#   tidytext::scale_x_reordered()
-
 marker_pct <- all_markers %>%
+  # Preprocess data
   mutate(diff = pct.2 - pct.1) %>%
   pivot_longer(c(pct.1, pct.2), names_to = 'pct.type', values_to = 'pct') %>%
-  mutate(ID = tidytext::reorder_within(ID, diff, types)) %>%
+  mutate(ID = tidytext::reorder_within(ID, diff, types)) %>% # Sort within facets (needs tidytext::scale_x_reordered)
+  # Make base plot
   ggplot() + 
   geom_col(aes(x = ID, y = pct, fill = pct.type), position = 'dodge') + 
   facet_wrap(~types, scales = 'free_y', nrow = 3) +
+  # Adjust theme
   theme_bw() + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.25),
         strip.background = element_rect(fill = 'white'),
